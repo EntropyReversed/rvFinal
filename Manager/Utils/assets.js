4cf5dd3db0dd3a5ed17ffb94bcd31529ab0cc21b
EntropyReversed <39929232+EntropyReversed@users.noreply.github.com>
2022-12-20 23:06:09 +0100

    1 file modified

--- Manager/Utils/assets.js
+++ Manager/Utils/assets.js
@@ -2,16 +2,11 @@ export default [
   {
     name: 'model',
     type: 'glbModel',
-    path: '/rv2.glb',
+    path: '/final.glb',
   },
   // {
   //   name: 'model',
   //   type: 'glbModel',
-  //   path: '/rvModel2.glb',
-  // },
-  // {
-  //   name: 'lettersTexture',
-  //   type: 'image',
-  //   path: '/texture3.png',
+  //   path: '/rv2.glb',
   // },
 ];

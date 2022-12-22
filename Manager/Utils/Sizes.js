298dd9be8bc448286e8ba9f499120fef1adbd2e5
EntropyReversed <39929232+EntropyReversed@users.noreply.github.com>
2022-12-22 17:15:22 +0100

    2 files modified

--- Manager/Utils/Sizes.js
+++ Manager/Utils/Sizes.js
@@ -6,6 +6,14 @@ export default class Sizes {
     // this.aspectSpan.style.top = '0';
     // this.aspectSpan.style.left = '0';
     // this.aspectSpan.style.color = 'white';
+
+    // this.sizeSpan = document.createElement('span');
+    // document.body.appendChild(this.sizeSpan);
+    // this.sizeSpan.style.position = 'fixed';
+    // this.sizeSpan.style.top = '30px';
+    // this.sizeSpan.style.left = '0';
+    // this.sizeSpan.style.color = 'white';
+
     this.setSizes();
   }
 
@@ -15,10 +23,10 @@ export default class Sizes {
     this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
     // this.aspectSpan.innerHTML = this.aspect;
+    // this.sizeSpan.innerHTML = `${this.width} ${this.height}`;
   }
 
   resize() {
-    // this.aspectSpan.innerHTML = this.aspect;
     this.setSizes();
   }
 }
